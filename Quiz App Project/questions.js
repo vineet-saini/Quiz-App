4938794081185fc6d25d7164b6c77108ae637b87
vineet-saini <123089775+vineet-saini@users.noreply.github.com>
2024-04-29 12:46:48 +0530

    Add files via upload

---
+++ Quiz App Project/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "A. Cascading Style Sheets",
+        options: [
+            "A. Cascading Style Sheets",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stands for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
